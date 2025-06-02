b46ab73bf6ee39b5c58120fa2138aa63998d88c5
Kotlovskiy <YA111EP@mail.ru>
2025-06-02 14:27:07 +0700

    UI
    Всё та же кнопка

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class MainMenu : Control
 {
+	
+	private Button _startButton;
+
+	public override void _Ready()
+	{
+		_startButton = GetNode<Button>("Button");
+		
+		_startButton.Connect("pressed", this, nameof(OnStartButtonPressed));
+	}
+
+	private void OnStartButtonPressed()
+	{
+		GD.Print("Кнопка 'Пуск' нажата!");
+		GetTree().ChangeScene("res://GameScene.tscn");
+	}
+	
 }
